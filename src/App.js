366c91867ad2b6893c76aa4f3b223a93aa984ca1
Carlos Zamora <czamora5@ucol.mx>
2022-09-21 04:20:26 -0500

    style: add custom properties and change color palette

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 // Components
+import Navbar from './components/Navbar/Navbar.js';
 import Home from './pages/Home/Home.js';
 import Albums from './pages/Albums/Albums.js';
 
@@ -11,8 +12,10 @@ import './style.scss';
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/albums" element={<Albums />} />
+      <Route path="/" element={<Navbar />}>
+        <Route index element={<Home />} />
+        <Route path="albums" element={<Albums />} />
+      </Route>
     </Routes>
   );
 }
